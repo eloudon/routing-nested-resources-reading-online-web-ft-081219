9f0df108f9c9c094059177e510c2d6c97bb0f687
Eric Loudon <loudon.eric@gmail.com>
2019-11-05 16:26:15 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,15 @@
 class AuthorsController < ApplicationController
 
+   def index
+    if params[:author_id]
+      @posts = Author.find(params[:author_id]).posts
+    else
+      @posts = Post.all
+    end
+  end
+
   def show
-    @author = Author.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def posts_index
